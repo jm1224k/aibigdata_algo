8dc226638cc43dda6c122686804ea02e37a02648
jm1224k <38150578+jm1224k@users.noreply.github.com>
2019-10-09 10:37:16 +0900

    Create vps.cpp

---
+++ 1009/vps.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string solve(string s)
+{
+	int cnt = 0;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '(')
+			cnt++;
+		else cnt--;
+
+		if (cnt < 0)
+			return "NO";
+	}
+
+	if (cnt == 0)
+		return "YES";
+	else
+		return "NO";
+}
+
+int main(void) {
+
+	int T;
+	cin >> T;
+
+	string s;
+
+	while (T--)
+	{
+		cin >> s;
+		cout << solve(s) << endl;
+	}
+
+	return 0;
+
+}
