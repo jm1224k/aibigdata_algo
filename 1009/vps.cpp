89f8f56f0c1d1e13e46273d54f53170803192829
jm1224k <38150578+jm1224k@users.noreply.github.com>
2019-10-09 10:41:39 +0900

    Update vps.cpp

--- 1009/vps.cpp
+++ 1009/vps.cpp
@@ -5,17 +5,15 @@ using namespace std;
 string solve(string s)
 {
 	int cnt = 0;
-
 	for (int i = 0; i < s.size(); i++)
 	{
 		if (s[i] == '(')
 			cnt++;
-		else cnt--;
-
+		else
+			cnt--;
 		if (cnt < 0)
 			return "NO";
 	}
-
 	if (cnt == 0)
 		return "YES";
 	else
@@ -26,7 +24,6 @@ int main(void) {
 
 	int T;
 	cin >> T;
-
 	string s;
 
 	while (T--)
@@ -34,7 +31,5 @@ int main(void) {
 		cin >> s;
 		cout << solve(s) << endl;
 	}
-
 	return 0;
-
 }
